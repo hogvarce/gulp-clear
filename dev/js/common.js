ab5584e719247b468815805e21c880a6953863e8
nukkunaana <nuochi@yandex.ru>
2016-02-02 10:45:12 +0300

    fix jquery

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,7 +1,17 @@
 requirejs.config({
     baseUrl: '/assets/js/',
     paths: {
+        jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
         helperPage : 'helper-page'
+    },
+    shim: {
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'helperPage': {
+            deps: ['jquery'],
+            exports: 'helperPage'
+        }
     }
 });
 
